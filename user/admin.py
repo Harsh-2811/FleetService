20c56c11557eabaae9b28f7d03e63981dd362d05
Harsh Patel <harshpatel@Harshs-MacBook-Pro.local>
2024-10-10 11:26:39 +0530

    Remove unwanted changes

--- user/admin.py
+++ user/admin.py
@@ -4,6 +4,8 @@ from .models import *
 
 # Register your models here.
 class CustomeUserAdmin(UserAdmin):
-    pass
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Driver Info", {'fields': ('is_driver', 'first_name', 'last_name')}),
+    )
 
-admin.site.register(User,CustomeUserAdmin)
+admin.site.register(User, CustomeUserAdmin)
