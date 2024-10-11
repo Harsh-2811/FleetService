8706fa5015844453998a33b209e7e8bf8db1a5f4
Mihir_Patel <19it104@charusat.edu.in>
2024-10-11 16:30:02 +0530

    user model return udpate

--- user/models.py
+++ user/models.py
@@ -5,10 +5,5 @@ from django.contrib.auth.models import AbstractUser,Group
 class User(AbstractUser):
     is_driver=models.BooleanField(default=True)
 
-
     def __str__(self):
-<<<<<<< HEAD
         return f"{self.username}-{self.first_name}"
-=======
-        return self.username
->>>>>>> 6f6d7cd023491d6a39f4b3d4203f41e8d853486a
