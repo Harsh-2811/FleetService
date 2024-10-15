79aa6a3a2a3d7b01ebf899169a50fe7d329e164a
Harsh Patel <harshpatel@Harshs-MacBook-Pro.local>
2024-10-15 08:07:40 +0530

    Add New APIS

--- general/urls.py
+++ general/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     path('',views.FormFields.as_view(),name="form_fields"),
     path('support_persons/',views.SupportPersons.as_view(),name="support_persons"),
+    path('contact_us/',views.ContactUs.as_view(),name="contact_us"),
 ]
