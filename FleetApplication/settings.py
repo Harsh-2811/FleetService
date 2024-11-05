59c81f7eb2ef87a9561b25bd60dac8760014b5d7
Harsh Patel <harshpatel@Harshs-MacBook-Pro.local>
2024-11-05 08:16:50 +0530

    Change Display List

--- FleetApplication/settings.py
+++ FleetApplication/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'fleet_service',
-        'USER': 'postgres',
+        'USER': 'fleet_service',
         'PASSWORD': 'Harsh@2811',
         'HOST': 'localhost',
         'PORT': '5432'
